4e864a6a8ab2b83624bc76a065c50c1f596ed86c
Prashant <prashantrahul141@protonmail.com>
2022-11-14 19:05:09 +0530

    adding playbackState class and small changes

---
+++ src/playbackState.ts
@@ -0,0 +1,19 @@
+import util from 'util';
+
+export module PlaybackState {
+  // Will store all the data related to playback/playing video.
+  export class PlaybackStateClass {
+    // Constructor
+    constructor(
+      public playState: boolean = false,
+      public playbackTime: string = '00:00',
+      public playbackSpeed: string = '1.00',
+      public videofileTitle: string = 'None'
+    ) {}
+
+    // custom log statement.
+    [util.inspect.custom]() {
+      return `PlayState : ${this.playState}\nPlaybackTime : ${this.playbackTime}\nPlaybackSpeed : ${this.playbackSpeed}\nVideoFileTitle : ${this.videofileTitle}`;
+    }
+  }
+}
