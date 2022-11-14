ff827cd9c962986dd55bc6914f9b4a2699d46862
Prashant <prashantrahul141@protonmail.com>
2022-11-14 23:47:57 +0530

    added check for every request if it exists in connections, changed return messages

--- src/lib/callbackHandler.ts
+++ src/lib/callbackHandler.ts
@@ -7,6 +7,13 @@ let playbackState = new PlaybackStateClass();
 // connectID (string) : hasTOUpdate (bool) .
 let connections: any = {};
 
+const ifExitsInConnections = (req: Request, res: Response) => {
+  const connectID = req.params.connectID;
+  if (connections[connectID] == undefined) {
+    connections[connectID] = true;
+  }
+};
+
 const updateConnections = (exception: string) => {
   // will set all connection's hasToUpdate True except for the one
   // who made changes.
@@ -21,17 +28,21 @@ export const connect = (req: Request, res: Response) => {
   // Adds new viewers
   const connectID = req.params.connectID;
   if (connections[connectID] !== undefined) {
-    res.status(200).json('Already Connected.');
+    res
+      .status(200)
+      .json({ state: 'already connected', file: playbackState.videofilename });
   } else {
     connections[connectID] = true;
-    res.status(200).json('New User Connected.');
+    res
+      .status(201)
+      .json({ state: 'new connected', file: playbackState.videofilename });
   }
-  console.log(connections);
 };
 
 export const getAll = (req: Request, res: Response) => {
   // Returns all playback info
   const connectID = req.params.connectID;
+  ifExitsInConnections(req, res);
   if (connections[connectID]) {
     // if changes are made
     connections[connectID] = false;
@@ -44,44 +55,54 @@ export const getAll = (req: Request, res: Response) => {
 
 export const playPause = (req: Request, res: Response) => {
   // for pause and play
+  ifExitsInConnections(req, res);
   const connectID = req.params.connectID;
   updateConnections(connectID);
   playbackState.playState = !playbackState.playState;
-  res.status(200).json(playbackState.playState);
+  res.status(200).json({ state: 'Success', ...playbackState });
 };
 
 export const setTime = (req: Request, res: Response) => {
   // to update time
+  ifExitsInConnections(req, res);
   const connectID = req.params.connectID;
   updateConnections(connectID);
   const time = req.params.time;
   if (!time) {
-    res.status(418).send('Time parameter was empty.');
+    res
+      .status(418)
+      .json({ state: 'Failure', message: 'Time parameter was empty.' });
   }
   playbackState.playbackTime = time;
-  res.status(200).json(playbackState.playbackTime);
+  res.status(200).json({ state: 'Success', ...playbackState });
 };
 
 export const setPlaybackSpeed = (req: Request, res: Response) => {
   // to update playback speed
+  ifExitsInConnections(req, res);
   const connectID = req.params.connectID;
   updateConnections(connectID);
   const speed = req.params.speed;
   if (!speed) {
-    res.status(418).json('Speed parameter was empty.');
+    res
+      .status(418)
+      .json({ state: 'Failure', message: 'Speed parameter was empty.' });
   }
   playbackState.playbackSpeed = speed;
-  res.status(200).json(playbackState.playbackSpeed);
+  res.status(200).json({ state: 'Success', ...playbackState });
 };
 
 export const setFileName = (req: Request, res: Response) => {
   // to set filename
+  ifExitsInConnections(req, res);
   const connectID = req.params.connectID;
   updateConnections(connectID);
   const filename = req.params.filename;
   if (!filename) {
-    res.status(418).json('FIlename parameter was empty.');
+    res
+      .status(418)
+      .json({ state: 'Failure', message: 'Filename parameter was empty.' });
   }
   playbackState.videofilename = filename;
-  res.status(200).json(playbackState.videofilename);
+  res.status(200).json({ state: 'Success', ...playbackState });
 };
